d021c212c191a1c6b413b9d82113431630014603
GopalABD28 <krishnaviru1999@gmail.com>
2026-09-09 22:02:17 +0530

    Add Main class with basic print and math operations

---
+++ app.java
@@ -0,0 +1,15 @@
+// Save this file as Main.java
+public class Main {
+    public static void main(String[] args) {
+        // 1. Print a simple message to the screen
+        System.out.println("Hello, World!");
+
+        // 2. Perform a basic math operation
+        int number1 = 10;
+        int number2 = 20;
+        int sum = number1 + number2;
+
+        // 3. Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
